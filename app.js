754d0164fc240de4e72de5224378b66fd8299f5f
Guilherme Fiola <guilherme.fiola@consinco.com.br>
2018-12-20 13:41:51 -0200

    Javascript Server

--- app.js
+++ app.js
@@ -10,16 +10,28 @@ const server = http.createServer((req, res) => {
     res.write("<html>");
     res.write("<head><title>Enter message</title></head>");
     res.write(
-      '<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>'
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write("</html>");
     return res.end();
   }
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
+    const body = [];
+    
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
 
   res.setHeader("Content-Type", "text/html");
