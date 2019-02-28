7695525da31d5e9310aaecb21e06ec4e12e5855d
Guilherme Fiola <guilherme.fiola@consinco.com.br>
2019-02-28 18:54:36 -0300

    Commit implementação Pug / Handlebars / EJS

--- routes/admin.js
+++ routes/admin.js
@@ -6,13 +6,29 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  //Handlebars
+  res.render('add-product', {
+    pageTitle: 'Add Product', 
+    path: '/admin/add-product', 
+    activeAddProduct: true,
+    formsCss: true,
+    productCss: true
+   });
+  
+  //Pug
+  //res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
+  
+  //HTML
+  //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
   
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  products.push({title: req.body.title});
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,25 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const adminData = require('./admin');
+
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  
+  // Handlebars
+  res.render('shop', { 
+    products: products, 
+    pageTitle: 'Shop', 
+    path: '/', 
+    activeShop: true, 
+    productCss: true
+  });
+
+  // Pug
+  //res.render('shop', { products: products, pageTitle: 'Shop', path: '/' });
+  
+  // HTML
+  //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
