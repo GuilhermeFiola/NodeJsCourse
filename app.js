ad9113174b45803fb903e3eeafbe9b3d1c98d4a2
Guilherme Fiola <guilherme.fiola@consinco.com.br>
2019-03-07 13:54:34 -0300

    Controller e Model

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const errorController = require('./controllers/error');
+
+const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRouter);
 app.use(shopRouter);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', {pageTitle: 'Page Not Found', path: ''});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,9 @@
-const path = require('path');
-
 const express = require('express');
-
-const rootDir = require('../util/path');
 
 const router = express.Router();
 
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', { products: products, pageTitle: 'Shop', path: '/' });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
