013a385ae7cd889f5b8127aafb66032da09e27fc
GuilhermeFiola <guilhermefiola@gmail.com>
2019-07-29 22:53:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(errorController.get404);
 
 mongoose
     .connect(
-        'mongodb+srv://Guilherme:zpD5ScxssyCBx56D@nodejscluster-1oaa4.mongodb.net/shop?retryWrites=true'
+        ''
     )
     .then(result => {
         User.findOne().then(user => {
