17a64505fc0077c4106ceca65e0f872739cbb3a1
Guilherme Fiola <guilhermefiola@gmail.com>
2019-08-26 22:42:24 -0300

    Término de autenticação básica

--- app.js
+++ app.js
@@ -36,7 +36,11 @@ app.use(
 );
 
 app.use((req, res, next) => {
-    User.findById('5d2e71de4cfcc3466c8389e4')
+    if (!req.session.user) {
+        return next();
+    }
+
+    User.findById(req.session.user._id)
         .then(user => {
             req.user = user;
             next();
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         pageTitle: 'Login',
@@ -7,6 +9,21 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true;
-    res.redirect('/');
+    User.findById('5d2e71de4cfcc3466c8389e4')
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            req.session.save(err => {
+                console.log(err);
+                res.redirect('/');
+            });
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    });
 };
